3578cab7154d467f3cfb8991aecc6948c179e722
ezequielfrizon <ezqlcwb@hotmail.com>
2023-07-13 17:15:45 -0300

    Added add user and edit user components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import Home from "../views/Home.vue";
+import UserAdd from "../views/UserAdd.vue";
+import UserEdit from "../views/UserEdit.vue";
 
 Vue.use(VueRouter)
 
@@ -9,6 +12,16 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/user-add",
+    name: "useradd",
+    component: UserAdd,
+  },
+  {
+    path: "/users/:userId",
+    name: "useredit",
+    component: UserEdit,
   },
 ];
 
