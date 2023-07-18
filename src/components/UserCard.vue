fbafe4a7e3aeb048f5035c9a6ec4e39f700e6055
ezequielfrizon <ezqlcwb@hotmail.com>
2023-07-18 00:03:16 -0300

    bugfix: there was a bug when editing a user, I took the opportunity to break the home into smaller parts

---
+++ src/components/UserCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-card-title class="custom__card-title-menu">
+      <span class="text-h5">Usuário logado: {{ userName }}</span>
+      <v-spacer></v-spacer>
+      <user-menu></user-menu>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+import UserMenu from "./UserMenu.vue";
+
+export default {
+  components: {
+    UserMenu,
+  },
+  props: {
+    userName: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.custom__card-title-menu {
+  color: #ffffff !important;
+  background-color: #515151;
+  border-radius: 0px !important;
+}
+</style>
---
+++ src/components/UserMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-menu bottom left>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn dark icon v-bind="attrs" v-on="on">
+        <v-icon>mdi mdi-menu</v-icon>
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item>
+        <v-btn dark normal text color="#694EDF" @click="logout">
+          Sair
+        </v-btn>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+import { signOut } from "firebase/auth";
+
+export default {
+  methods: {
+    logout() {
+      signOut(auth)
+        .then(() => {})
+        .catch((err) => alert(err.message));
+    },
+  },
+};
+</script>
